49f4e76463687f23ff625701b7dc450fa99e71ed
LuisLunaV <luis.a.luna.v@outlook.com>
2023-01-09 03:44:00 -0600

    Se agregan middlewares de express y personalizados

--- router/user.routes.js
+++ router/user.routes.js
@@ -1,13 +1,20 @@
 const { Router } = require('express');
+const { check }  = require('express-validator');
 
 const {
     crearUsuario,
     actualizarUsuario
 } = require('../controllers/user.controller.js');
 
+const { validarCampos } = require('../middlewares/validar-campos.js');
+
 const router = Router();
 
-router.post('/',   crearUsuario);
+router.post('/', [
+check('Usuario_Nombre', 'Evite utilizar numeros y signos').matches(/^[a-zA-Z ]+$/),
+validarCampos
+], crearUsuario);
+
 router.put('/:id', actualizarUsuario);
 
 module.exports = router;

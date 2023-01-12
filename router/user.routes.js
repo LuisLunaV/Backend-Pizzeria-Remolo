dc5aad2b86287161e3842b77deb7feb1418da5e5
LuisLunaV <luis.a.luna.v@outlook.com>
2023-01-12 00:36:13 -0600

    Se agregan comentarios en el route de usuarios

--- router/user.routes.js
+++ router/user.routes.js
@@ -12,11 +12,13 @@ const { existeClientePorID } = require('../helpers/db-validator.js');
 
 const router = Router();
 
+//Buscamos el cliente por id
 router.get('/:id',[
 check('id').custom( existeClientePorID ),
 validarCampos
 ], buscarUsuarioPorId );
 
+//Creamos el cliente en la BD
 router.post('/', [
 check('Usuario_Nombre', 'Evite utilizar numeros y signos').matches(/^[a-zA-Z ]+$/),
 check('Usuario_Telefono','Evite utilizar letras y signos, el rango del numero debe ser entre min:10 y max:14').matches(/^[0-9]+$/).isLength( { min:10, max:14 } ),
@@ -24,6 +26,7 @@ check('Usuario_Direccion', 'Agregue una direccion de entrega').not().isEmpty(),
 validarCampos
 ], crearUsuario);
 
+//Editamos la informacion del usuario
 router.put('/:id',[
     check('id').custom( existeClientePorID )
 ], actualizarUsuario);

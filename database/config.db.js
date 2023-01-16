94b8250794fe0e89e29df487284ad898621369a7
LuisLunaV <luis.a.luna.v@outlook.com>
2023-01-16 02:05:21 -0600

    Se cambia la conexion de BD por URL

--- database/config.db.js
+++ database/config.db.js
@@ -1,14 +1,16 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize( process.env.NAME_DB, process.env.DB_USER , process.env.DB_PASS, {
-host: process.env.DB_HOST,
-dialect: 'mysql'
+// const db = new Sequelize( process.env.NAME_DB, process.env.DB_USER , process.env.DB_PASS, {
+// host: process.env.DB_HOST,
+// dialect: 'mysql'
 // dialectOptions: {
 //     ssl: {
 //         rejectUnauthorized: true,  
 //     }
 // }
-});
+// });
+
+const db = new Sequelize( 'mysql://root:qjfvIQmS3y8wIdyxSdbJ@containers-us-west-191.railway.app:5759/railway' );
 
 const dbConnection = async()=>{
     try {
